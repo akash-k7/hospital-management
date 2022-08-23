9a3ac7a53aee0e7daa89600696490a53f4d907c6
akash-k7 <92331024+akash-k7@users.noreply.github.com>
2022-08-23 19:34:42 +0530

    Update models.py

--- models.py
+++ models.py
@@ -77,6 +77,3 @@ class PatientDischargeDetails(models.Model):
     total=models.PositiveIntegerField(null=False)
 
 
-#Developed By : sumit kumar
-#facebook : fb.com/sumit.luv
-#Youtube :youtube.com/lazycoders
