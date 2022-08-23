6b398e0392d4c9b296717496f2fab4f5a992c454
akash-k7 <92331024+akash-k7@users.noreply.github.com>
2022-08-23 19:34:04 +0530

    Update admin.py

--- admin.py
+++ admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from .models import Doctor,Patient,Appointment,PatientDischargeDetails
-# Register your models here.
+
 class DoctorAdmin(admin.ModelAdmin):
     pass
 admin.site.register(Doctor, DoctorAdmin)
